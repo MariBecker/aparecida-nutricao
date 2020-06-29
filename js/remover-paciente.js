833cd8b5c43772334dfb8b0f9d817cb90677828e
Mari Becker <mary12_becker@hotmail.com>
2020-06-29 14:13:32 -0300

    Deixando código mais limpo

--- js/remover-paciente.js
+++ js/remover-paciente.js
@@ -3,8 +3,6 @@ var pacientes = document.querySelectorAll(".paciente");
 var tabela = document.querySelector("table");
 
 tabela.addEventListener("dblclick", function (event) {
-  var alvoEvento = event.target;
-  var paiDoAlvo = alvoEvento.parentNode; //TR = paciente = remover
-  paiDoAlvo.remove();
+  event.target.parentNode.remove();
 });
 
